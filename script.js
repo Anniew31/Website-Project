149196625e28ba2aa827e0474e310a2188dbc313
computer <annieweng34@gmail.com>
2025-02-05 21:18:42 -0500

    getting species by ISO now working

--- script.js
+++ script.js
@@ -15,10 +15,12 @@ function displayMap() {
 
   map.on("click", async function (e) {
     var country = await convertLocation(e.latlng.lat, e.latlng.lng);
-    
+    var ISO = await convertLocationISO(e.latlng.lat, e.latlng.lng);
+    var speciesName = await getSpecies(ISO);
+  
     popup
       .setLatLng(e.latlng)
-      .setContent(`Country: ${country}`)
+      .setContent(`<b>A Endangered Species in ${country} (${ISO}):</b><br>${speciesName}`)
       .openOn(map);
   });
 }
@@ -38,7 +40,40 @@ async function convertLocation(lat, lon) {
   }
 }
 
-// get species name 
-async function getSpecies() {}
+// get country ISO from coordinates
+async function convertLocationISO(lat, lon) {
 
+  var response = await fetch(
+    `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`
+  );
+  var data = await response.json();
+    
+  if (data && data.address && data.address.country_code) {
+    return data.address.country_code.toUpperCase();
+  } else {
+      return null;
+  }
+}
+
+// get species name 
+async function getSpecies(region) {
+  var apiURL = `https://apiv3.iucnredlist.org/api/v3/country/getspecies/${region.toString()}?token=9bb4facb6d23f48efbf424bb05c0c1ef1cf6f468393bc745d42179ac4aca5fee`;
+
+  try {
+    var response = await fetch(apiURL);
+    var data = await response.json();
   
+    if (data.count > 0) {
+      return result = data.result[0].scientific_name;
+    } else {
+        return ["No endangered species found here"];
+    }
+  } catch (error) {
+      return ["Error fetching endangered species data"]
+  }
+}
+
+/* APIs used: 
+- IUCN 2024. IUCN Red List of Threatened Species. Version 2024-2 <www.iucnredlist.org>
+
+*/
